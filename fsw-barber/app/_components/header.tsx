46c4310c67d6577c79153faa3f3c9aaf30db29de
Luciano Silva <luciano_jet@hotmail.com>
2024-08-14 15:12:37 -0300

    feat: adicionado heards componetes e banner

---
+++ fsw-barber/app/_components/header.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+    return ( 
+        <Card>
+            <CardContent className="justify-between flex flex-row items-center p-5">
+                <Image src="/logo.png" alt="FSW Barber" width={120} height={18} />
+                <Button size="icon" variant="outline">
+                    <MenuIcon/>
+                </Button>
+
+            </CardContent>
+        </Card>
+
+
+     );
+}
+ 
+export default Header
--- fsw-barber/app/page.tsx
+++ fsw-barber/app/page.tsx
@@ -1,10 +1,56 @@
-import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
+import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 const Home = () => {
-  return ( 
-      <Button>
-         teste
-      </Button>
+   return (
+      <div>
+         {/* HEADER */}
+         <Header />
+
+         {/* TEXTO SAUDAÇÃO */}
+
+         <div className="p-5">
+            <h2 className="text-xl font-bold">Olá, Fulustreco</h2>
+            <p>Quarta-Feira, 14 de Agosto</p>
+
+            {/* BARRA DE PESQUISA */}
+
+            <div className="gap-2 mt-6 flex items-center">
+               <Input placeholder="Busque Aqui..." />
+               <Button>
+                  <SearchIcon />
+               </Button>
+
+            </div>
+
+            {/* BANNER PRINCIPAL */}
+
+            <div className="relative mt-6 h-[150px] w-full">
+               <Image
+                  alt="Banner principal"
+                  src="/banner-01.png"
+                  fill
+                  className="rounded-xl object-cover"
+               />
+            </div>
+
+            {/* AGENDAMENTOS */}
+
+            
+
+
+
+         </div>
+
+
+
+
+      </div>
+
+
    );
 }
 
