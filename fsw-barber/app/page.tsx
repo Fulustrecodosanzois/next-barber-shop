a425ff11665e95a5efea6e2e6b966640ac65a036
Luciano Silva <luciano_jet@hotmail.com>
2024-08-14 18:31:51 -0300

    feat: adicionado o item bookig icone e componentizado

--- fsw-barber/app/page.tsx
+++ fsw-barber/app/page.tsx
@@ -3,13 +3,12 @@ import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
-import { Card, CardContent } from "./_components/ui/card";
-import { Badge } from "./_components/ui/badge";
-import { Avatar } from "./_components/ui/avatar";
-import { AvatarImage } from "@radix-ui/react-avatar";
 import { db } from "./_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
 import Footer from "./_components/footer";
+import { quickSearchOptions } from "./_constants/search";
+import BookingItem from "./_components/booking-item";
+
 
 const Home = async () => {
    // chamar banco
@@ -44,77 +43,22 @@ const Home = async () => {
 
             {/* BUSCA RÁPIDA */}
 
+            { }
+
             <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-               <Button className="gap-2" variant="secondary">
-                  <Image
-                     src="/cabelo.svg"
-                     width={16}
-                     height={16}
-                     alt="cabelo"
-                  />
-                  Cabelo
-               </Button>
 
-               <Button className="gap-2" variant="secondary">
-                  <Image
-                     src="/barba.svg"
-                     width={16}
-                     height={16}
-                     alt="barba"
-                  />
-                  Barba
-               </Button>
+               {quickSearchOptions.map((option) => (
+                  <Button className="gap-2" variant="secondary" key={option.title}>
+                     <Image
+                        src={option.imageUrl}
+                        width={16}
+                        height={16}
+                        alt={option.title}
 
-               <Button className="gap-2" variant="secondary">
-                  <Image
-                     src="/acabamento.svg"
-                     width={16}
-                     height={16}
-                     alt="acabamento"
-                  />
-                  Acabamento
-               </Button>
-
-               <Button className="gap-2" variant="secondary">
-                  <Image
-                     src="/massagem.svg"
-                     width={16}
-                     height={16}
-                     alt="massagem"
-                  />
-                  Massagem
-               </Button>
-
-               <Button className="gap-2" variant="secondary">
-                  <Image
-                     src="/sobramcelha.svg"
-                     width={16}
-                     height={16}
-                     alt="sobrancelha"
-                  />
-                  Sobrancelha
-               </Button>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                     />
+                     {option.title}
+                  </Button>
+               ))}
 
             </div>
 
@@ -130,35 +74,8 @@ const Home = async () => {
             </div>
 
             {/* AGENDAMENTOS */}
-            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-               AGENDAMENTOS
-            </h2>
 
-            <Card>
-               <CardContent className="flex justify-between p-0">
-                  {/* ESQUERDA */}
-                  <div className="flex flex-col gap-2 py-5 pl-5">
-                     <Badge className="w-fit">Confirmado</Badge>
-                     <h3 className="font-semibold">Corte de Cabelo</h3>
-
-                     <div className="flex items-center gap-2">
-                        <Avatar className="h-6 w-6">
-                           <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                        </Avatar>
-                        <p className="text-sm">Barbearia FSW</p>
-                     </div>
-                  </div>
-
-                  {/* DIREITA */}
-
-                  <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-                     <p className="text-sm">Agosto</p>
-                     <p className="text-2xl">05</p>
-                     <p className="text-sm">15:00</p>
-
-                  </div>
-               </CardContent>
-            </Card>
+            <BookingItem />
 
 
             {/* RECOMENDADOS */}
