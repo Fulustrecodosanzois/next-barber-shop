b0a788bafccd01f2ac46ab0f2ea6bc0756b9028c
Luciano Silva <luciano_jet@hotmail.com>
2024-08-20 12:17:26 -0300

    chore: adicionado authnext

--- fsw-barber/app/_components/sidebar-sheet.tsx
+++ fsw-barber/app/_components/sidebar-sheet.tsx
@@ -1,10 +1,11 @@
 import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
 import { Button } from "./ui/button";
-import { Avatar, AvatarImage } from "./ui/avatar";
+// import { Avatar, AvatarImage } from "./ui/avatar";
 import { quickSearchOptions } from "../_constants/search";
 import Image from "next/image";
 import Link from "next/link";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
 
 const SidebarSheet = () => {
     return (
@@ -15,7 +16,41 @@ const SidebarSheet = () => {
             </SheetHeader>
 
             <div className="flex items-center gap-3 border-b border-solid py-5">
-                <Avatar>
+                <h2 className="font-bold">Olá, Faça seu login</h2>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button size="icon">
+                            <LogOutIcon />
+                        </Button>
+                    </DialogTrigger>
+                    <DialogContent className="w-[90%]">
+                        <DialogHeader>
+                            <DialogTitle>Faça login na plataforma</DialogTitle>
+                            <DialogDescription>
+                                Conecte-se usando uma plataforma do Google
+                            </DialogDescription>
+                        </DialogHeader>
+                        <Button variant="outline" className="gap-1 font-bold">
+                            <Image 
+                            src="/google.svg"
+                            alt="Login Google"
+                            width={18}
+                            height={18}
+                        
+                            />
+                            Google
+                        </Button>
+                    </DialogContent>
+                </Dialog>
+
+
+
+
+
+
+
+
+                {/* <Avatar>
                     <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
                 </Avatar>
 
@@ -23,7 +58,7 @@ const SidebarSheet = () => {
                     <p className="font-bold">Fulustreco Dos Anzois</p>
                     <p className="text-xs">Fulus@fulus.com</p>
 
-                </div>
+                </div> */}
             </div>
 
             <div className="flex flex-col gap-4 border-b border-solid py-5">
