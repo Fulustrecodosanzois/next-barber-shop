49330730bde6f4ebc764c9662896d48bf2f29dac
Luciano Silva <luciano_jet@hotmail.com>
2024-08-20 10:28:06 -0300

    feat: adicionado sidebar

--- fsw-barber/app/_components/header.tsx
+++ fsw-barber/app/_components/header.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
-import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import SidebarButton from "./sidebar-button";
 
 const Header = () => {
-    return ( 
+    return (
         <Card>
             <CardContent className="justify-between flex flex-row items-center p-5">
                 <Image src="/logo.png" alt="FSW Barber" width={120} height={18} />
-                <Button size="icon" variant="outline">
-                    <MenuIcon/>
-                </Button>
+
+                <SidebarButton />
+
+
+
 
             </CardContent>
         </Card>
 
 
-     );
+    );
 }
- 
+
 export default Header
--- fsw-barber/app/barbershops/[id]/page.tsx
+++ fsw-barber/app/barbershops/[id]/page.tsx
@@ -1,5 +1,6 @@
 import PhoneItem from "@/app/_components/phone-item";
 import ServiceItem from "@/app/_components/service-item";
+import SidebarButton from "@/app/_components/sidebar-button";
 import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
@@ -63,7 +64,7 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
                     variant="secondary"
                     className="absolute right-4 top-4"
                 >
-                    <MenuIcon />
+                    <SidebarButton/>
                 </Button>
 
 
